44ad888c3ac13f7bd4ec928e1018981e9c828f1f
taufiqirawan <taufiqkipot@gmail.com>
2020-08-28 09:31:43 +0700

    initial commit

---
+++ src/Presenter.kt
@@ -0,0 +1,13 @@
+class Presenter {
+
+    fun selisih(number1 : Int, number2 : Int) : Int{
+        if (number1 < number2){
+            return number2 - number1
+        }else {
+            return number1 - number2
+        }
+    }
+
+    fun
+
+}
